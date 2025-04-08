575650bebd4ce4be760e1d66c105174168e9d83d
Sam Meydanshahi <sam.meydan7@gmail.com>
2025-04-08 01:23:11 -0400

    updated resume

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import pic from './resources/1-cb2185ca.jpg'
 import github from './resources/github.png'
 import linkedin from './resources/hd-square-black-outline-linkedin-icon-png-7017516950455535cziiy18li.png'
-import resume from './resources/Mar2025u_Resume.pdf'
+import resume from './resources/Apr2025_Resume.pdf'
 import {
   VerticalTimeline,
   VerticalTimelineElement
